745a9229db69196bd668b51e0082f39ac0e8531d
User <william.dewitt@live.mercer.edu>
2017-04-01 20:29:41 -0400

    Javascript to python and back is a success!

--- test2/livemap.py
+++ test2/livemap.py
@@ -84,6 +84,7 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         self.write_message(u"You said: " + message)
+        print("You said: "+message)
 
     def on_close(self):
         print("WebSocket closed")
